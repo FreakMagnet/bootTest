2c1afd897ac3c53d823141d9377033996d0274e4
sonovavic <tigerbaram@gmail.com>
2019-02-05 18:27:12 +0900

    첫업로드

---
+++ src/main/java/matt/practice/FreakMagnetApplication.java
@@ -0,0 +1,14 @@
+package matt.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreakMagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreakMagnetApplication.class, args);
+	}
+
+}
+
